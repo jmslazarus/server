95fbf4b3673c60cdf572c338c3ff599c16907963
jmslazarus <jmslazarus@yahoo.com>
2025-02-05 19:51:21 -0800

    testing

--- api/index.js
+++ api/index.js
@@ -12,14 +12,59 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
 	res.status(200).send('<h1>about page</h1>');
 });
-websockserver = new ws.Server({ 
-  // server: https.createServer({
-  //   key: fs.readFileSync('key.pem'),
-  //   cert: fs.readFileSync('cert.pem')
-  // }),
+
+const eof = function() {
+  ws.send('<EOL>'); 
+}
+
+const message = function(data, flags) {
+  var message = data.toString("utf-8"); 
+      split = message.split(' '),
+      command = split[0];
+  
+  console.log('[TerminalServer] Message: ', message);
+  switch (command) {
+    case "shit": 
+      ws.send('right now the only command is "help" and it\'s not very helpful'); 
+      eof();
+      break;
+    case "help": 
+      ws.send('you get no help'); 
+      eof();
+      break;
+    case "": 
+      ws.send(''); 
+      eof();
+      break;
+    case "generate": 
+      ws.send('<br>');
+      ws.send('_.o[ Internet Proceedural Generation Script ]o._<br><br>');
+      // this.initdb();
+      // this.generate(split[1], split[2]);
+      break;
+    default: 
+      ws.send("'" + command + "' is not a valid command"); 
+      eof();
+      break;
+  }
+}
+const connected = function(ws, req) {
+  console.log('[TerminalServer] Connected: ', req.socket.remoteAddress);
+  ws.send('established.<br>');
+
+  eof();
+
+  ws.on('message', function (data, flags) { message(data, flags) });
+}
+
+const websockserver = new ws.Server({ 
   port: 443,
   path: '/terminal'
 });
+
+websockserver.on('connection', function (ws, req) { connected(ws, req) });
+console.log('[TerminalServer] Listening for websockets');
+
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
